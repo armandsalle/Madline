f4dc119e5df0c80300e1ec1b930f7ab47c4308b1
Armand Sallé <armand.salle@gmail.com>
2020-05-08 12:19:18 +0200

    🚨Hot fix layout

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,17 +1,17 @@
-const React = require("react")
-const { registerLinkResolver } = require("gatsby-source-prismic-graphql")
-const { linkResolver } = require("./src/utils/linkResolver")
-const Layout = require("./src/hoc/layout").default
-const { LayoutProvider } = require("./src/context/layoutContext")
-const { SeoProvider } = require("./src/context/seoContext")
+import React from "react"
+import { registerLinkResolver } from "gatsby-source-prismic-graphql"
+import { linkResolver } from "./src/utils/linkResolver"
+import Layout from "./src/hoc/layout"
+import { LayoutProvider } from "./src/context/layoutContext"
+import { SeoProvider } from "./src/context/seoContext"
 
 registerLinkResolver(linkResolver)
 
-exports.onRouteUpdate = ({ location, prevLocation }) => {
+export const onRouteUpdate = ({ location, prevLocation }) => {
   document.querySelector("body").classList.remove("modal-open")
 }
 
-exports.wrapPageElement = ({ element, props }) => {
+export const wrapPageElement = ({ element, props }) => {
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
   return (
